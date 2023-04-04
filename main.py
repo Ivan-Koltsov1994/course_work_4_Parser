5ffb8d49bac6fcaa3a656fe369d825769265a5a1
Ivan <ivan.koltsov.1994@mail.com>
2023-04-04 22:01:54 +0500

    add main

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import os
 
 from project_classes.engine import Engine, HH, SuperJob
-from project_utils.utils import get_top_vacancies_by_date, get_top_vacancies,get_vacancies,check_vacancy
+from project_utils.utils import get_top_vacancies_by_date, get_top_vacancies, check_vacancy, get_info
 
 
 def main():
-
-    # путь к файлу с хранилишем вакансий
+    # путь к файлу с хранилищем вакансий
     path = os.path.join('project_data/all.json')
     connector = Engine.get_connector(path)
 
     print('Добрый день! Вы подключились к хранилищу вакансий.')
     keyword = input("Какую вакансию желаете посмотреть?\n").lower()
 
-    #  Отслеживаем данные о вакансии на HH,SJ
+    #  Отслеживаем данные о вакансии на HH, SJ
     hh = HH(keyword)
     sj = SuperJob(keyword)
 
-    #Проверяем статус получения данных
+    # Проверяем статус получения данных
     data_hh, info_hh = hh.get_request()  # Выводим статус получения данных
     data_sj, info_sj = sj.get_request()  # Выводим статус получения данных
     if not data_hh:
@@ -28,7 +27,7 @@ def main():
         print(f'{info_hh} c сайта HeadHunter')
         print(f'{info_hh} c сайта SuperJob')
 
-    #Складываем вакансии в файл или выводим исключение
+    # Складываем вакансии в файл или выводим исключение
     if check_vacancy(hh, sj):
         all_vacancies = hh.get_vacancies_list() + sj.get_vacancies_list()
         connector.insert(all_vacancies)
@@ -36,16 +35,54 @@ def main():
         print('Такой вакансии нет')
 
     # Сортируем полученные вакансии для вывода
+
     while True:
-        top_count = input('Укажите какое количество вакансий будем выводить на экран\n')
-        if not top_count.isdigit() or int(top_count) <= 0:
-            print('Количество должно быть целым числом больше ноля. Попробуйте еще раз')
+        count_vacancy = input('Введите количество выводимых вакансий\n')
+        if not count_vacancy.isdigit() or int(count_vacancy) <= 0:
+            print('Количество вакансий не может быть меньше 0. Необходимо повторить попытку')
             continue
         else:
-            top_count = int(top_count)
+            count_vacancy = int(count_vacancy)
             break
 
     vacancies = connector.select({})
+
+    # Вывод данных в меню пользователя
+    while True:
+        print(f'Вашему вниманию предлагаются следующие варианты выбора вакансий:\n\
+                        1 - вывести {count_vacancy} последних вакансии\n\
+                        2 - вывести топ-{count_vacancy} вакансий заработку\n\
+                        stop - закончить работу')
+
+        user_input = input("Введите нужный вариант\n")
+
+        if user_input == '1':
+            data = get_top_vacancies_by_date(vacancies, count_vacancy)
+            get_info(data)
+
+        elif user_input == '2':
+            data = get_top_vacancies(vacancies, count_vacancy)
+            get_info(data)
+
+        elif user_input.lower() == 'stop':
+            print('Программа завершает работу')
+            break
+
+        else:
+            print("Такого варианта нет, попробуйте еще раз")
+            continue
+
+        print('Вам нужны еще варианты выбора вакансий? Yes/None')
+
+        # Предлагаем дополнительное пользование парсером
+        keyword_user = input().upper()
+
+        if keyword_user == 'Yes':
+            continue
+        else:
+            print('Спасибо,что пользуетесь нашим приложением!')
+            break
+
 
 if __name__ == '__main__':
     main()
